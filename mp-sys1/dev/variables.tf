c71e5c4e7ef347295b823355579a46dcf8f79cf7
mikayp1967 <root@ip-172-31-21-92.eu-west-2.compute.internal>
2019-12-07 13:30:45 +0000

    My own module

---
+++ mp-sys1/dev/variables.tf
@@ -0,0 +1,47 @@
+variable "cidr_vpc" {
+    default = "192.168.0.0/16"
+   # 192.168.0.0 - 192.168.255.255
+}
+
+variable "cidr_instance_tenancy" {
+    default = "dedicated"
+  
+}
+
+
+variable "cidr_subnet" {
+    #192.168.1.0 - 192.168.1.255
+    default = "192.168.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "eu-west-2a"
+}
+
+
+
+
+variable "ami" {
+  default = "ami-05f37c3995fffb4fd"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_name" {
+  default = ""
+}
+
+variable "vpc_id" {
+  #default = {}
+  default = "vpc-055b547b95c79c9d1"
+}
+
+variable "subnet_id" {
+  #default = {}
+  default = "subnet-0d44efe1e45c20546"
+}
+
+
+
