c0f2a57e3313ff50484a0a59f187957132890548
EC2 Default User <ec2-user@ip-172-31-15-63.eu-west-2.compute.internal>
2021-11-05 23:14:55 +0000

    Extend to create subnets for each AZ

--- 3tier/vpc.tf
+++ 3tier/vpc.tf
@@ -4,5 +4,6 @@ module "BE_VPC" {
   cidr = "10.0.1.0/24"
   subnet_cidr = ["10.0.1.0/26", "10.0.1.64/26", "10.0.1.128/26"]
   name = "backend"
+  az_list = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 }
 
