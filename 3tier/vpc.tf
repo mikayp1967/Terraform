88e817feb5528ed4a70b92168b6c3f3d2d951bcc
Michele P <mikayp1967@gmail.com>
2021-11-06 00:26:21 +0000

    Move module to more logical location

--- 3tier/vpc.tf
+++ 3tier/vpc.tf
@@ -1,5 +1,5 @@
 module "BE_VPC" {
-  source      = "modules/vpc"
+  source      = "../modules/vpc"
   cidr        = "10.0.1.0/24"
   subnet_cidr = ["10.0.1.0/26", "10.0.1.64/26", "10.0.1.128/26"]
   name        = "backend"
