1dec849f57f6197ed6a22156d23d9641e46dda39
MrP <mikayp1967@gmail.com>
2021-11-13 23:06:40 +0000

    Add IAM role to allow ec2 to read S3 bucket for keys

--- kube/ec2.tf
+++ kube/ec2.tf
@@ -33,7 +33,7 @@ module "ec2_instance" {
   key_name               = var.key_name
   vpc_security_group_ids = [module.K8_VPC.default_security_group_id]
   subnet_id              = element(module.K8_VPC.subnets, 0)
-  #user_data = "sudo apt-get update"
+  iam_instance_profile   = aws_iam_instance_profile.Kube_S3_profile.name
   user_data = <<-EOF
     #!/bin/bash
     sudo apt-get update
@@ -65,3 +65,47 @@ resource "aws_eip" "cp_eip" {
 output "CP_ip" {
   value = aws_eip.cp_eip.public_ip
 }
+
+
+
+# Create role for EC2 and attach relevant policies
+resource "aws_iam_role" "CP_IAM_S3" {
+  name = "Kube_Node_IAM_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "s3_read"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["s3:GetObject"]
+          Effect   = "Allow"
+          #Resource = "arn:aws:s3:::key-store-bucket-390490349038000/*"
+          Resource = format("arn:aws:s3:::%s/*",var.key_bucket)
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_iam_instance_profile" "Kube_S3_profile" {
+  name = "Kube_S3_profile"
+  role = "${aws_iam_role.CP_IAM_S3.name}"
+}
+
+
