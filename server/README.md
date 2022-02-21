2508a064dbe349b0dd048a9b6ad02fbbb8afca86
MrP <mikayp1967@gmail.com>
2022-02-21 09:03:49 +0000

    Basic ec2 config with S3 bucket

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# Simple server/S3 example - just a few staples of AWS
+
+## ToDo
+
+* S3 endpoint so server can communicate with S3
+* EIP to login to the server
+* Add a VPC, SN, Route tables, SG etc 
+* Add ssh key to server
+* Maybe do something with it - userdata, ASG, install web server or something
+
---
+++ server/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "key-store-bucket-390490349038000"
+    key    = "server-state-files/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
+
